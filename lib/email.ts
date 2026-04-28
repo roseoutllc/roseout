158b46da07c06b37b1041b811001b96ec36ef9da
flockpulse <nick@flockpulse.com>
2026-04-28 17:37:13 -0400

    go

--- lib/email.ts
+++ lib/email.ts
@@ -1,6 +1,10 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+if (!process.env.RESEND_API_KEY) {
+  throw new Error("Missing RESEND_API_KEY");
+}
+
+export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({
   to,
