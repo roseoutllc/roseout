6c5dc9b8a5ee864ec5725c82e47ba976a9966834
flockpulse <nick@flockpulse.com>
2026-05-01 09:39:37 -0400

    g

--- app/owner/dashboard/page.tsx
+++ app/owner/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function OwnerDashboardRedirectPage() {
+export default function OwnerDashboardPage() {
   redirect("/locations/dashboard");
 }
